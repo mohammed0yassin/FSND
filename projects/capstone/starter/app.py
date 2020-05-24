33fbe1f9033e3017b8399ea1bc2b4ae5b8448357
Mohammed Yassin <7037909+mohammed0yassin@users.noreply.github.com>
2020-05-24 05:13:13 +0200

    -Added app=create_app() in app.py/ Added postman collection to test production API

--- projects/capstone/starter/app.py
+++ projects/capstone/starter/app.py
@@ -381,6 +381,6 @@ def create_app(test_config=None):
       }), error_status_code
   return app
 
-
+app = create_app()
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
