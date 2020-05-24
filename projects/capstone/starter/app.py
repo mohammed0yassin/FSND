019786753f0d6199483482087c803236a6b6a55c
Mohammed Yassin <7037909+mohammed0yassin@users.noreply.github.com>
2020-05-24 02:00:35 +0200

    - Code now adheres to the PEP 8 style guide

--- projects/capstone/starter/app.py
+++ projects/capstone/starter/app.py
@@ -12,13 +12,20 @@ def list_shows():
   it lists all the shows of the agencys
   returns shows which is a dictionary that has the shows
 '''
+
+
 def list_shows():
-  no_shows_all = db.session.query(Show.movie_id).distinct().count() # number of shows
-  movies_in_shows = [value for value, in db.session.query(Show.movie_id).distinct().all()]
+  # number of shows
+  no_shows_all = db.session.query(Show.movie_id).distinct().count()
+  movies_not_formatted = db.session.query(Show.movie_id).distinct().all()
+  movies_in_shows = [value for value, in movies_not_formatted]
   shows = []
   for cur_show in range(no_shows_all):
-    cur_movie = Movie.query.get(movies_in_shows[cur_show]) # current movie
-    cur_actors = db.session.query(Actor).join(Show).filter(Show.movie_id==cur_movie.id).all() # actors in the current movie
+    # current movie
+    cur_movie = Movie.query.get(movies_in_shows[cur_show])
+    # actors in the current movie
+    cur_actors = db.session.query(Actor).join(Show) \
+                   .filter(Show.movie_id == cur_movie.id).all()
     actor_record = []
     for actor in range(len(cur_actors)):
       current_actor = cur_actors[actor]
@@ -30,7 +37,6 @@ def list_shows():
       }
       actor_record.append(actor_info)
 
-
     record = {
       'movie_title': cur_movie.title,
       'movie_id': cur_movie.id,
@@ -40,7 +46,7 @@ def list_shows():
     shows.append(record)
   return shows
 
-    
+
 def create_app(test_config=None):
   # create and configure the app
   app = Flask(__name__)
@@ -49,13 +55,12 @@ def create_app(test_config=None):
 
   CORS(app, resources={r"*": {"origins": "*"}})
 
-
   @app.after_request
   def after_request(response):
       response.headers.add('Access-Control-Allow-Headers',
-                          'Content-Type,Authorization,true')
+                           'Content-Type,Authorization,true')
       response.headers.add('Access-Control-Allow-Methods',
-                          'GET,PUT,POST,DELETE,PATCH')
+                           'GET,PUT,POST,DELETE,PATCH')
       return response
 
   '''
@@ -117,7 +122,7 @@ def create_app(test_config=None):
         'age': body['age'],
         'gender': body['gender']
       }
-      
+
       new_actor = Actor(**actor_data)
       new_actor.insert()
 
@@ -131,7 +136,6 @@ def create_app(test_config=None):
     except:
       abort(422)
 
-
   '''
   POST '/movies': An endpoint that adds a new movie
     requires permission 'post:movies'
@@ -141,14 +145,15 @@ def create_app(test_config=None):
   def add_movie(payload):
     try:
       body = request.get_json()
-      # release_date format: '%Y-%m-%d %H:%M:%S' # example: "2019-05-21 21:30:00"
+    # release_date format: '%Y-%m-%d %H:%M:%S' #example: "2019-05-21 21:30:00"
       date_not_formatted = body['release_date']
-      date_formatted = datetime.strptime(date_not_formatted,'%Y-%m-%d %H:%M:%S')
+      date_formatted = datetime.strptime(date_not_formatted,
+                                         '%Y-%m-%d %H:%M:%S')
       movie_data = {
         'title': body['title'],
         'release_date': date_formatted
       }
-      
+
       new_movie = Movie(**movie_data)
       new_movie.insert()
 
@@ -161,7 +166,6 @@ def create_app(test_config=None):
       })
     except:
       abort(422)
-
 
   '''
   POST '/shows': an end point to add a new show
@@ -177,15 +181,16 @@ def create_app(test_config=None):
         'actors_ids': body['actors_ids']
       }
       # check if a show with this movie already exists
-      check_movie = Show.query.filter(Show.movie_id == show_data['movie_id']).all()
-      if check_movie: #if check_movie has a movie
+      check_movie = Show.query.filter(Show.movie_id ==
+                                      show_data['movie_id']).all()
+      if check_movie:  # if check_movie has a movie
         abort(400)
       no_of_actors = len(show_data['actors_ids'])
       for actor in range(no_of_actors):
-        new_show = Show(movie_id = show_data['movie_id'],
-                        actor_id = show_data['actors_ids'][actor])
+        new_show = Show(movie_id=show_data['movie_id'],
+                        actor_id=show_data['actors_ids'][actor])
         new_show.insert()
-      
+
       shows = list_shows()
 
       return jsonify({
@@ -194,8 +199,6 @@ def create_app(test_config=None):
       })
     except:
       abort(422)
-    
-
 
   '''
   PATCH '/actors/<id>': An endpoint to edit an actor
@@ -209,7 +212,7 @@ def create_app(test_config=None):
       new_name = body.get('name', None)
       new_age = body.get('age', None)
       new_gender = body.get('gender', None)
-      
+
       current_actor = Actor.query.get(id)
 
       if new_name is not None:
@@ -232,7 +235,6 @@ def create_app(test_config=None):
     except:
       abort(422)
 
-
   '''
   PATCH '/movies/<id>': An endpoint to edit a movie
     requires permission 'patch:movies'
@@ -243,15 +245,16 @@ def create_app(test_config=None):
     try:
       body = request.get_json()
       new_title = body.get('title', None)
-      # release_date format: '%Y-%m-%d %H:%M:%S' # example: "2019-05-21 21:30:00"
+    # release_date format: '%Y-%m-%d %H:%M:%S' #example: "2019-05-21 21:30:00"
       date_not_formatted = body.get('release_date', None)
-      
+
       current_movie = Movie.query.get(id)
 
       if new_title is not None:
         current_movie.title = new_title
       if date_not_formatted is not None:
-        date_formatted = datetime.strptime(date_not_formatted,'%Y-%m-%d %H:%M:%S')
+        date_formatted = datetime.strptime(date_not_formatted,
+                                           '%Y-%m-%d %H:%M:%S')
         current_movie.release_date = date_formatted
 
       current_movie.update()
@@ -266,7 +269,6 @@ def create_app(test_config=None):
       })
     except:
       abort(422)
-  
 
   '''
   DELETE '/actors/<id>': an endpoint to delete an actor
@@ -276,10 +278,10 @@ def create_app(test_config=None):
   @requires_auth('delete:actors')
   def delete_actor(payload, id):
     select_actor = Actor.query.get(id)
-    
+
     if select_actor is None:
       abort(404)
-      
+
     select_actor.delete()
 
     actors_all = Actor.query.all()
@@ -290,7 +292,6 @@ def create_app(test_config=None):
       'deleted': id,
       'actors': actors
     })
-
 
   '''
   DELETE '/movies/<id>': an endpoint to delete an movie
@@ -307,13 +308,12 @@ def create_app(test_config=None):
 
     movies_all = Movie.query.all()
     movies = [movie.format() for movie in movies_all]
-    
+
     return jsonify({
       'success': True,
       'deleted': id,
       'movies': movies
     })
-
 
   '''
   DELETE '/shows/<movie_id>': an endpoint to delete a show using its movie_id
@@ -323,7 +323,7 @@ def create_app(test_config=None):
   @requires_auth('delete:shows')
   def delete_show(payload, movie_id):
     selected_show = Show.query.filter(Show.movie_id == movie_id).all()
-    if not selected_show: # if no shows with this movie exist, abort
+    if not selected_show:  # if no shows with this movie exist, abort
       abort(404)
     for show in selected_show:
       show.delete()
@@ -334,7 +334,6 @@ def create_app(test_config=None):
       'deleted': movie_id,
       'shows': shows
     })
-    
 
   '''
   Error handlers
@@ -371,7 +370,6 @@ def create_app(test_config=None):
       'message': 'Internal Server Error'
     }), 500
 
-
   @app.errorhandler(AuthError)
   def autherror(error):
       error_details = error.error
@@ -382,11 +380,6 @@ def create_app(test_config=None):
           'message': error_details['description']
       }), error_status_code
   return app
-
-
-
-
-
 
 
 if __name__ == '__main__':
